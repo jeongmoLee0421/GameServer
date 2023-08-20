6cb763176b73d9debc7eb085f97e68e76bdb1b8d
jeongmoLee0421 <62637776+jeongmoLee0421@users.noreply.github.com>
2023-08-20 14:46:10 +0900

    thread class
    일정 시간(tick)마다 해야 할 작업(OnProcess)을 수행하는 thread

---
+++ Practice/NetworkLibrary/Thread.cpp
@@ -0,0 +1,154 @@
+﻿#define _WINSOCKAPI_
+#include <Windows.h>
+#include <cassert>
+#include <process.h>
+
+#include "Thread.h"
+#include "Log.h"
+
+Thread::Thread()
+	: mThread{ NULL }
+	, mIsRunning{ false }
+	, mWaitTick{ 0 }
+	, mTickCount{ 0 }
+{
+	mQuitEvent = CreateEvent(
+		NULL,
+		true, // QuitEvent가 세팅되어 Signaled 상태가 되면, thread가 종료될거라서 굳이 자동 reset이 필요하지는 않다.
+		false,
+		NULL
+	);
+
+	// mQuitEvent는 참이라고 주장한다.
+	// 그니까 NULL이면 std::abort가 호출되어 프로그램을 중단
+	assert(mQuitEvent);
+}
+
+Thread::~Thread()
+{
+	if (mQuitEvent)
+	{
+		CloseHandle(mQuitEvent);
+	}
+
+	if (mThread)
+	{
+		CloseHandle(mThread);
+	}
+}
+
+unsigned int WINAPI CallTickThread(LPVOID p)
+{
+	Thread* pThread = reinterpret_cast<Thread*>(p);
+
+	pThread->TickThread();
+
+	return 0;
+}
+
+bool Thread::CreateThread(DWORD waitTick)
+{
+	unsigned int threadID{ 0 };
+
+	mThread = reinterpret_cast<HANDLE>(_beginthreadex(
+		NULL,
+		0,
+		CallTickThread,
+		this, // this 포인터를 넘겨서 멤버 함수를 호출해준다.
+		CREATE_SUSPENDED, // 이후에 내가 원하는 타이밍에 thread 진행 가능
+		&threadID
+	));
+
+	if (NULL == mThread)
+	{
+		LOG(LOG_ERROR_NORMAL,
+			"SYSTEM | Thread::CreateThread() | TickThread 생성 실패: Error(%lu)",
+			GetLastError());
+
+		return false;
+	}
+
+	mWaitTick = waitTick;
+	return true;
+}
+
+void Thread::DestroyThread()
+{
+	// event 객체를 signaled 상태로 변경하더라도
+	// thread가 일시정지한 상태라면,
+	// WaitForSingleObject() 함수 호출이 불가하여
+	// event 객체의 상태를 감지할 수 없기 때문
+	Run();
+
+	// Quit Event를 signaled 상태로 변경하여
+	// WaitForSingleObject() 함수에서 이를 감지하면,
+	// 반환 값을 확인해서 while문 탈출하여 thread를 종료
+	SetEvent(mQuitEvent);
+
+	WaitForSingleObject(mThread, INFINITE);
+}
+
+void Thread::Run()
+{
+	if (false == mIsRunning)
+	{
+		mIsRunning = true;
+		ResumeThread(mThread);
+	}
+}
+
+void Thread::Stop()
+{
+	// 모던 C++에 들어와서는
+	// thread를 임의로 멈추거나 재개하는 함수를 제공하지 않았다.
+	// 그 이유는 동기화를 위한 lock 소유 문제가 있다.
+	//
+	// 예를 들어서
+	// 어떤 thread가 lock을 소유했는데
+	// 하필 그 순간에 thread 정지 함수가 호출되어서
+	// 아무런 작업을 하지 않는다면,
+	// 추후에 thread 재개 함수를 호출하여 정지되어 있는 thread를 깨우기 전까지
+	// lock을 필요로 하는 다른 thread도 작업 진행이 멈춰버리는 경우가 생긴다.
+	//
+	// 그래서 조건 변수 같은 것을 사용해서
+	// 수식이 거짓인 경우에는 계속 대기하고 있다가
+	// 수식이 참이면, 일어나서 추가 작업을 하는 등의 방법을
+	// 요즘에는 권장하고 있다.
+
+	if (true == mIsRunning)
+	{
+		mIsRunning = false;
+		SuspendThread(mThread);
+	}
+}
+
+void Thread::TickThread()
+{
+	while (true)
+	{
+		DWORD ret = WaitForSingleObject(mQuitEvent, mWaitTick);
+
+		// event 객체가 signaled 상태가 되었다
+		if (WAIT_OBJECT_0 == ret)
+		{
+			break;
+		}
+		// 시간 제한이 경과했고
+		// non-signaled 상태
+		else if (WAIT_TIMEOUT)
+		{
+			++mTickCount;
+			OnProcess();
+		}
+	}
+}
+
+DWORD Thread::GetTickCount()
+{
+	return mTickCount;
+}
+
+bool Thread::IsRunning()
+{
+	return mIsRunning;
+}
---
+++ Practice/NetworkLibrary/Thread.h
@@ -0,0 +1,61 @@
+﻿#pragma once
+
+// 2023 08 20 이정모 home
+
+// 일정 시간마다 특정 작업을 하기 위한 thread class
+// 
+// tick마다 client에게 패킷을 전송하여
+// 해당 client가 연결되어있는지(살아있는지) 여부를 체크하거나
+// 
+// 온라인 게임 서버 동기화는 서버 시간을 기준으로 하는데
+// server tick이란 단위를 만들어서 처리할 수도 있다.
+
+#ifdef NETWORKLIBRARY_EXPORTS
+#define NETLIB_API __declspec(dllexport)
+#else
+#define NETLIB_API __declspec(dllimport)
+#endif
+
+#define _WINSOCKAPI_
+#include <Windows.h>
+
+class NETLIB_API Thread
+{
+public:
+	Thread();
+	~Thread();
+
+public:
+	bool CreateThread(DWORD waitTick);
+	void DestroyThread();
+	void Run();
+	void Stop();
+
+public:
+	// 한 주기(tick)가 지나면
+	// 해야할 작업이 구현된 OnProcess() 함수를 호출해준다.
+	void TickThread();
+
+	// OnProcess() 함수를 순수 가상 함수로 선언해서
+	// Thread class를 상속하는 모든 자식 class는 이를 반드시 재정의해야 함.
+	// 
+	// 결국 자식 thread마다 해야할 작업들이 다르게 재정의 될 것이고
+	// 부모 thread로 Up Casting 하여 OnProcess() 함수를 호출하면,
+	// 가상 함수 테이블을 참조하여
+	// 자식 thread에서 구현된 OnProcess() 함수를 호출한다.
+	virtual void OnProcess() = 0;
+
+	// 주기가 몇번 반복되었나?
+	DWORD GetTickCount();
+	bool IsRunning();
+
+	// 자식 class에서 사용할 필요가 있을 수도 있기에 protected
+protected:
+	HANDLE mThread;
+	HANDLE mQuitEvent;
+
+	bool mIsRunning;
+	
+	DWORD mWaitTick;
+	DWORD mTickCount;
+};
