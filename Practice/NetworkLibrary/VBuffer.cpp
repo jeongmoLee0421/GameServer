ecd0ffbac3095f30ef9d34abb7c0eb63d489ee82
jeongmoLee0421 <62637776+jeongmoLee0421@users.noreply.github.com>
2023-08-21 17:48:35 +0900

    가변 패킷 처리하는 VBuffer class 추가
    패킷의 규격이 정해져 있는 고정 패킷도 있지만,
    server나 client의 상황에 따라서 구성되는 데이터가 다른 가변 패킷도 필요하다.
    가변 패킷을 만들기 위해 데이터를 세팅하고
    가변 패킷을 해석하기 위해 데이터를 추출하는 class이다.

---
+++ Practice/NetworkLibrary/VBuffer.cpp
@@ -0,0 +1,177 @@
+﻿#define _WINSOCKAPI_
+#include <Windows.h>
+
+#include "VBuffer.h"
+#include "Singleton.h"
+
+constexpr int MAX_VBUFFER_SIZE = 1024 * 50;
+constexpr int MAX_PBUFSIZE = 4096; // PacketPool에서 버퍼 한개당 size라는데 아직은 잘 모름
+constexpr int PACKET_SIZE_LENGTH = 4;
+
+IMPLEMENT_SINGLETON(VBuffer);
+
+void VBuffer::Initialize()
+{
+	mVBuffer = new char[MAX_VBUFFER_SIZE] {};
+	mMaxBufSize = MAX_VBUFFER_SIZE;
+
+	PrepareForDataSetting();
+}
+
+void VBuffer::Finalize()
+{
+	delete[] mVBuffer;
+}
+
+void VBuffer::GetChar(char& ch)
+{
+	ch = *mCurrentMark;
+
+	mCurrentMark += 1;
+}
+
+void VBuffer::GetShort(short& num)
+{
+	// 가장 아래 바이트를 읽어오고
+	num = *mCurrentMark +
+		// 그 다음 바이트를 읽어오는데
+		// 8비트를 밀어서 원래 위치(두번째 바이트)를 복구
+		(*(mCurrentMark + 1) << 8);
+
+	mCurrentMark += 2;
+}
+
+void VBuffer::GetInteger(int& num)
+{
+	num = *mCurrentMark +
+		(*(mCurrentMark + 1) << 8) +
+		(*(mCurrentMark + 2) << 16) +
+		(*(mCurrentMark + 3) << 24);
+
+	mCurrentMark += 4;
+}
+
+void VBuffer::GetStream(char* pBuffer, short length)
+{
+	if (0 > length || MAX_PBUFSIZE < length)
+	{
+		return;
+	}
+
+	// 문자열이 아니고
+	// 바이트 스트림이기 때문에
+	// 바이트 단위로 복사
+	CopyMemory(pBuffer, mCurrentMark, length);
+
+	mCurrentMark += length;
+}
+
+void VBuffer::GetString(char* pBuffer)
+{
+	short length{ 0 };
+	GetShort(length);
+
+	if (0 > length || MAX_PBUFSIZE < length)
+	{
+		return;
+	}
+
+	CopyMemory(pBuffer, mCurrentMark, length);
+
+	// 문자열을 추출을 CopyMemory(memcpy)로 수행했기 때문에
+	// 뒤에 널문자를 붙여주자
+	*(pBuffer + length) = '\0';
+
+	mCurrentMark += length;
+}
+
+void VBuffer::SetChar(char ch)
+{
+	*mCurrentMark = ch;
+
+	mCurrentMark += 1;
+
+	// 내가 몇 바이트를 세팅했는지 누적해서 계산해두고
+	// 데이터 세팅이 완료되면,
+	// 외부 송신 버퍼에 복사할 때
+	// 세팅한 총 바이트만큼 복사하기 위함
+	mCurrentBufSize += 1;
+}
+
+void VBuffer::SetShort(short num)
+{
+	// 가장 아래 바이트 세팅
+	*mCurrentMark = static_cast<char>(num);
+
+	// 왼쪽으로 8비트 밀어서
+	// 두번째 바이트 세팅
+	*(mCurrentMark + 1) = num >> 8;
+
+	mCurrentMark += 2;
+	mCurrentBufSize += 2;
+}
+
+void VBuffer::SetInteger(int num)
+{
+	*mCurrentMark = static_cast<char>(num);
+	*(mCurrentMark + 1) = num >> 8;
+	*(mCurrentMark + 2) = num >> 16;
+	*(mCurrentMark + 3) = num >> 24;
+
+	mCurrentMark += 4;
+	mCurrentBufSize += 4;
+}
+
+void VBuffer::SetStream(char* pBuffer, short length)
+{
+	CopyMemory(mCurrentMark, pBuffer, length);
+
+	mCurrentMark += length;
+	mCurrentBufSize += length;
+}
+
+void VBuffer::SetString(char* pBuffer)
+{
+	short length = static_cast<short>(strlen(pBuffer));
+	
+	if (0 > length || MAX_PBUFSIZE < length)
+	{
+		return;
+	}
+	
+	// 문자열을 세팅할 때는
+	// 선두 2바이트에 길이 정보를 넣는다.
+	SetShort(length);
+
+	CopyMemory(mCurrentMark, pBuffer, length);
+
+	mCurrentMark += length;
+	mCurrentBufSize += length;
+}
+
+void VBuffer::SetBuffer(char* pVBuffer)
+{
+	mCurrentMark = pVBuffer;
+}
+
+void VBuffer::PrepareForDataSetting()
+{
+	// 선두 4바이트는 패킷의 길이를 나타냄
+	mCurrentMark = mVBuffer + PACKET_SIZE_LENGTH;
+
+	// 패킷의 길이도 데이터를 세팅하는 것이기 때문에
+	// 현재 세팅된 버퍼 크기도 4바이트로 시작
+	mCurrentBufSize = PACKET_SIZE_LENGTH;
+}
+
+bool VBuffer::CopyBuffer(char* pDstBuffer)
+{
+	// 선두 4바이트에는 패킷의 전체 길이 정보를 저장
+	CopyMemory(mVBuffer, &mCurrentBufSize, PACKET_SIZE_LENGTH);
+
+	// 송신할 데이터 시작 위치부터
+	// 전체 길이 만큼 복사
+	CopyMemory(pDstBuffer, mVBuffer, mCurrentBufSize);
+
+	return true;
+}
---
+++ Practice/NetworkLibrary/VBuffer.h
@@ -0,0 +1,116 @@
+﻿#pragma once
+
+// 2023 08 20 이정모 home
+
+// 가변 길이 패킷을 처리하기위한 class
+
+// 고정 길이 패킷을 사용한다면,
+// 내가 송수신할 패킷의 크기가 명확하게 정해져있다는 뜻이고
+// 이는 구조체로써 표현이 가능하다.
+// 그래서 패킷의 크기만큼 수신하게 되면,
+// 구조체의 멤버 변수를 통해 데이터를 접근하기에 좋다.
+// 다만, 보낼 데이터가 적은 경우에도
+// 고정된 패킷의 크기만큼 전송해야 하기 떄문에
+// 불필요한 데이터 전송이 발생한다.
+
+// 가변 길이 패킷을 사용한다면,
+// 송신할 데이터의 양이 작으면, 패킷의 길이도 작아지고
+// 송신할 데이터의 양이 많으면, 패킷의 길이도 커진다.
+// 즉, 패킷 길이에 맞추어서 데이터를 전송하기 때문에
+// 효율적인 데이터 전송이 발생한다.
+// 다만, 패킷의 길이가 가변이라서
+// 패킷을 구조체로 정형화할 수 없기 때문에
+// 송신하는 쪽에서 데이터를 입력했을 때
+// 수신하는 쪽에서는 송신하는 쪽에서 입력한 데이터 순서대로 읽어야 하기 때문에
+// 처리가 조금 더 복잡하다.
+
+#ifdef NETWORKLIBRARY_EXPORTS
+#define NETLIB_API __declspec(dllexport)
+#else
+#define NETLIB_API __declspec(dllimport)
+#endif
+
+#include "Singleton.h"
+
+// Singleton class를 상속해서
+// 전역 위치 어디에서든 편하게 가변 길이 패킷을 처리하도록 했다.
+// 패킷 처리가 끝날 때까지
+// 데이터 버퍼를 가리키는 포인터가 변경되면 문제가 발생할 수 있는점 유의하자.
+// 당연히 패킷 처리하면서 데이터를 뽑아올 때는 lock을 걸어야 한다.
+class NETLIB_API VBuffer : public Singleton
+{
+	DECLEAR_SINGLETON(VBuffer);
+
+public:
+	// 외부에서 수신한 가변 패킷 데이터가 있을텐데
+	// 자료형만큼 데이터를 읽고
+	// 자료형만큼 버퍼 포인터를 미는 함수
+	void GetChar(char& ch);
+	void GetShort(short& num);
+	void GetInteger(int& num);
+	void GetStream(char* pBuffer, short length);
+
+	// 문자열을 읽는 함수로
+	// 선두 2바이트에 문자열의 길이 정보를 읽어야 한다.
+	void GetString(char* pBuffer);
+
+public:
+	// 내부 가변 버퍼에
+	// 필요한 데이터를 세팅하고
+	// 자료형만큼 버퍼 포인터를 미는 함수.
+	// 데이터 세팅이 완료되면, CopyBuffer()를 호출하여
+	// 데이터 송신을 위한 버퍼에 복사한다.
+	void SetChar(char ch);
+	void SetShort(short num);
+	void SetInteger(int num);
+	void SetStream(char* pBuffer, short length);
+
+	// 문자열을 세팅하는 함수로
+	// 선두 2바이트에 문자열의 길이 정보를 넣어야 한다.
+	void SetString(char* pBuffer);
+
+public:
+	// 데이터를 추출하기 위해
+	// 외부에서 수신한 가변 패킷 데이터가 담긴 버퍼의
+	// 시작 위치를 세팅
+	void SetBuffer(char* pVBuffer);
+
+	// 내부 가변 버퍼에
+	// 가변 데이터를 세팅하기 전에
+	// 필요한 작업
+	void PrepareForDataSetting();
+
+	// 송신할 데이터를
+	// 내부 가변 버퍼에 모두 세팅했으면,
+	// 실제 송신할 버퍼에 복사하는 함수
+	bool CopyBuffer(char* pDstBuffer);
+
+public:
+	int GetMaxBufSize();
+	int GetCurrentBufSize();
+	char* GetCurrentMark();
+	char* GetBeginMark();
+
+private:
+	// 데이터를 세팅할 실제 내부 버퍼 시작 포인터
+	char* mVBuffer;
+	
+	// 데이터를 추출할 때는 외부 버퍼의 위치를 가리키고
+	// 데이터를 세팅할 때는 내부 버퍼의 위치를 가리키는 포인터
+	char* mCurrentMark;
+
+	// 내부 버퍼 최대 크기
+	int mMaxBufSize;
+
+	// 내부 버퍼에 세팅한 데이터를
+	// 외부에 복사해야 할 때
+	// 얼마나 복사해야 하는지
+	// 버퍼 크기를 의미
+	int mCurrentBufSize;
+
+	VBuffer(const VBuffer& rhs) = delete;
+	VBuffer(VBuffer&& rhs) = delete;
+
+	VBuffer& operator=(const VBuffer& rhs) = delete;
+	VBuffer& operator=(VBuffer&& rhs) = delete;
+};
