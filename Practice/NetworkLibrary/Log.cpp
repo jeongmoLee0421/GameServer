9357546c219253964f160ce01997838eca66f747
jeongmoLee0421 <62637776+jeongmoLee0421@users.noreply.github.com>
2023-08-24 15:21:17 +0900

    log class 추가
    게임 서버 실행 중에 원하는 매체에 로그를 출력해주는 기능

---
+++ Practice/NetworkLibrary/Log.cpp
@@ -0,0 +1,646 @@
+﻿#include <time.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+#pragma comment(lib, "ws2_32")
+
+#include "Log.h"
+
+IMPLEMENT_SINGLETON(Log);
+
+// 생성자/소멸자가 private이라서
+// 외부에서 직접 호출이 불가능하기 때문에
+// Initialize(), Finalize() 함수에
+// 생성/소멸 시 해야할 작업을 넣으면,
+// 내부 생성자/소멸자에서 호출해줌
+void Log::Initialize()
+{
+
+}
+
+void Log::Finalize()
+{
+
+}
+
+bool Log::Init(LogConfig& logConfig)
+{
+	time_t currTime{ time(NULL) };
+	struct tm localTime {};
+	localtime_s(&localTime, &currTime);
+
+	// 어떤 매체에
+	// 어떤 등급의 로그를 출력할지에 대한 정보 세팅
+	CopyMemory(mLogInfoTypes,
+		logConfig.mLogInfoTypes,
+		MAX_STORAGE_TYPE * sizeof(int));
+
+	wchar_t strTime[100]{};
+	wcsftime(strTime,
+		sizeof(strTime),
+		L"%m월%d일%H시%M분",
+		&localTime);
+
+	// 로그를 출력할 디렉토리 생성
+	CreateDirectory(L"./LOG", NULL);
+
+	// 로그를 저장할 파일 이름 세팅
+	swprintf_s(mLogFileName,
+		sizeof(mLogFileName),
+		L"./Log/%s_%s.log",
+		logConfig.mLogFileName,
+		strTime);
+
+	// log class 멤버 변수에
+	// 로그 정보를 세팅
+	strncpy_s(mIP,
+		sizeof(mIP),
+		logConfig.mIP,
+		MAX_IP_LENGTH);
+
+	strncpy_s(mDSNName,
+		sizeof(mDSNName),
+		logConfig.mDSNName,
+		MAX_DSN_NAME);
+
+	strncpy_s(mDSNID,
+		sizeof(mDSNID),
+		logConfig.mDSNID,
+		MAX_DSN_ID);
+
+	strncpy_s(mDSNPW,
+		sizeof(mDSNPW),
+		logConfig.mDSNPW,
+		MAX_DSN_PW);
+
+	mLogFileType = logConfig.mLogFileType;
+	mTCPPort = logConfig.mTCPPort;
+	mUDPPort = logConfig.mUDPPort;
+	mFileMaxSize = logConfig.mFileMaxSize;
+	mHwnd = logConfig.mHwnd;
+
+	bool ret{ true };
+
+	// 로그를 출력할 매체가 파일인데
+	// LOG_NONE이 아니라는 것은
+	// 알람 또는 에러 정보를 남기겠다는 의미
+	if (static_cast<int>(eLogInfoType::LOG_NONE) !=
+		static_cast<int>(mLogInfoTypes[static_cast<int>(eLogStorageType::STORAGE_FILE)]))
+	{
+		ret = InitFile();
+	}
+	if (false == ret)
+	{
+		CloseAllLog();
+		return false;
+	}
+
+	// DB로 로그를 출력할건지?
+	if (static_cast<int>(eLogInfoType::LOG_NONE) !=
+		static_cast<int>(mLogInfoTypes[static_cast<int>(eLogStorageType::STORAGE_DB)]))
+	{
+		ret = InitDB();
+	}
+	if (false == ret)
+	{
+		CloseAllLog();
+		return false;
+	}
+
+	// UDP로 로그를 출력할건지?
+	if (static_cast<int>(eLogInfoType::LOG_NONE) !=
+		static_cast<int>(mLogInfoTypes[static_cast<int>(eLogStorageType::STORAGE_UDP)]))
+	{
+		ret = InitUDP();
+	}
+	if (false == ret)
+	{
+		CloseAllLog();
+		return false;
+	}
+
+	// TCP로 로그를 출력할건지?
+	if (static_cast<int>(eLogInfoType::LOG_NONE) !=
+		static_cast<int>(mLogInfoTypes[static_cast<int>(eLogStorageType::STORAGE_TCP)]))
+	{
+		ret = InitTCP();
+	}
+	if (false == ret)
+	{
+		CloseAllLog();
+		return false;
+	}
+
+	// 로그를 출력하기 위한 환경이 세팅되었다면,
+	// tick마다 전담해서 로그를 출력하기 위한 thread를 생성
+	CreateThread(logConfig.mProcessTick);
+	Run();
+	return true;
+}
+
+void Log::LogOutput(eLogInfoType logInfoType, wchar_t* outputString)
+{
+	// logInfoType에
+	// 로그의 종류(알람, 에러), 로그의 등급 정보가
+	// 비트로 저장되어 있는데
+	// and 연산을 했을 때 0이 아닌 값이 나왔다면,
+	// 해당 로그는 남겨야한다는 의미
+	if (mLogInfoTypes[static_cast<int>(eLogStorageType::STORAGE_UDP)] &
+		static_cast<int>(logInfoType))
+	{
+		OutputUDP(logInfoType, outputString);
+	}
+
+	if (mLogInfoTypes[static_cast<int>(eLogStorageType::STORAGE_TCP)] &
+		static_cast<int>(logInfoType))
+	{
+		OutputTCP(logInfoType, outputString);
+	}
+
+	int logInfoTypeIndex = static_cast<int>(logInfoType);
+
+
+	// 4비트를 왼쪽으로 밀었는데 0이 아니다?
+	// LOG_ERROR
+	if (0 != (static_cast<int>(logInfoType) >> 4))
+	{
+		logInfoTypeIndex = (static_cast<int>(logInfoType) >> 4) + 0x10 - 1;
+	}
+
+	// 위 if문을 수행하지 않았다면,
+	// 하위 4비트가 세팅되어 있다는 뜻이고
+	// LOG_INFO인 경우로
+	// index값 자체가 string table의 index에 매핑된다.
+
+	if (0 > logInfoTypeIndex || 31 < logInfoTypeIndex)
+	{
+		return;
+	}
+
+	time_t currTime{ time(NULL) };
+	struct tm localTime {};
+
+	localtime_s(&localTime, &currTime);
+
+	wchar_t timeStr[40]{};
+	wcsftime(timeStr, sizeof(timeStr), L"%Y/%m/%d(%H/%M/%S)", &localTime);
+
+	// 시간 | 정보 형태 | 정보 등급 | 사용자 로그
+	swprintf_s(mOutString,
+		static_cast<size_t>(sizeof(mOutString) * 0.5), // wchar_t는 sizeof를 하면 2바이트로 잡히기 때문에 버퍼 개수는 바이트 크기의 절반이다
+		L"%ws | %ws | %ws | %ws\r\n",
+		timeStr,
+		static_cast<int>(logInfoType) >> 4 ? L"에러" : L"정보",
+		LogInfoType_StringTable[logInfoTypeIndex],
+		outputString);
+
+	// 매체에 출력하고자 하는 로그 등급과
+	// 현재 출력하고자 하는 로그의 등급이 일치하는지
+	// bitwise and 연산을 통해 확인
+	// 동일한 위치에 있는 비트가 1, 1이면 결과 비트는 1로 세팅되는 것을 이용
+	if (mLogInfoTypes[static_cast<int>(eLogStorageType::STORAGE_FILE)] &
+		static_cast<int>(logInfoType))
+	{
+		OutputFile(mOutString);
+	}
+
+	if (mLogInfoTypes[static_cast<int>(eLogStorageType::STORAGE_DB)] &
+		static_cast<int>(logInfoType))
+	{
+		OutputDB(mOutString);
+	}
+
+	if (mLogInfoTypes[static_cast<int>(eLogStorageType::STORAGE_WINDOW)] &
+		static_cast<int>(logInfoType))
+	{
+		OutputWindow(logInfoType, mOutString);
+	}
+
+	if (mLogInfoTypes[static_cast<int>(eLogStorageType::STORAGE_OUTPUTWND)] &
+		static_cast<int>(logInfoType))
+	{
+		OutputDebugger(mOutString);
+	}
+}
+
+void Log::LogOutputLastErrorToMsgBox(wchar_t* outputString)
+{
+	DWORD lastError{ GetLastError() };
+	if (0 == lastError)
+	{
+		return;
+	}
+
+	LPVOID pDump{ nullptr };
+
+	// last error를 설명해주는 문자열을
+	// 시스템 테이블에서 찾아서
+	// OS가 내부적으로 메모리를 할당하면,
+	// pDump가 해당 문자열을 가리키도록 함
+	int result = FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL,
+		lastError,
+		MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US),
+		reinterpret_cast<LPWSTR>(pDump),
+		0,
+		NULL
+	);
+
+	swprintf_s(
+		gOutString,
+		static_cast<size_t>(sizeof(gOutString) * 0.5), // gOutString의 배열 개수인 MAX_OUTPUT_LENGTH로 해도 괜찮다.
+		L"에러위치: %ws\n에러번호: %d\n설명: %ws",
+		outputString,
+		lastError,
+		reinterpret_cast<wchar_t*>(pDump)
+	);
+
+	// 콘솔이면
+	printf("%ws\n", gOutString);
+
+	// 창이면
+	//MessageBox(NULL, gOutString, L"GetLastError", MB_OK);
+
+	// 에러에 대한 설명을 담은 버퍼를
+	// OS가 내부적으로 할당했고
+	// pDump로 해당 주소를 가리키고 있다.
+	// 다 사용했다면, 해제를 요청하는 것은 당연
+	if (result)
+	{
+		LocalFree(pDump);
+	}
+}
+
+void Log::CloseAllLog()
+{
+	ZeroMemory(mLogInfoTypes, MAX_STORAGE_TYPE * sizeof(int));
+	ZeroMemory(mLogFileName, MAX_FILENAME_LENGTH);
+
+	mLogFileType = eLogFileType::FILETYPE_NONE;
+
+	ZeroMemory(mIP, MAX_IP_LENGTH);
+	mUDPPort = DEFAULT_UDPPORT;
+	mTCPPort = DEFAULT_TCPPORT;
+
+	ZeroMemory(mDSNName, MAX_DSN_NAME);
+	ZeroMemory(mDSNID, MAX_DSN_ID);
+	ZeroMemory(mDSNPW, MAX_DSN_PW);
+
+	mHwnd = NULL;
+
+	// 로그를 출력하기 위한 파일이 열려있다면,
+	// 닫아주자
+	if (mLogFile)
+	{
+		CloseHandle(mLogFile);
+		mLogFile = NULL;
+	}
+
+	if (INVALID_SOCKET != mUDPSocket)
+	{
+		closesocket(mUDPSocket);
+		mUDPSocket = INVALID_SOCKET;
+	}
+
+	if (INVALID_SOCKET != mTCPSocket)
+	{
+		shutdown(mTCPSocket, SD_BOTH);
+		closesocket(mTCPSocket);
+		mTCPSocket = INVALID_SOCKET;
+	}
+
+	// thread 종료
+	Stop();
+}
+
+void Log::OnProcess()
+{
+	// tick마다 호출되면,
+	// logMsgQueue에 있는 데이터를 읽어서
+	// log를 출력
+
+	int logCount = mLogMsgQueue.GetCurrentSize();
+	for (int i = 0; i < logCount; ++i)
+	{
+		// 가져와서
+		LogMsg* pLogMsg = mLogMsgQueue.Front();
+
+		// 출력하고
+		LogOutput(pLogMsg->mLogInfoType,
+			pLogMsg->mOutputString);
+
+		// 데이터 빼주고
+		mLogMsgQueue.Pop();
+	}
+}
+
+void Log::SetHwnd(HWND hWnd)
+{
+	mHwnd = hWnd;
+}
+
+int Log::GetQueueSize()
+{
+	return mLogMsgQueue.GetCurrentSize();
+}
+
+bool Log::InitFile()
+{
+	mLogFile = CreateFile(
+		mLogFileName,
+		GENERIC_WRITE,
+		FILE_SHARE_READ,
+		NULL,
+		OPEN_ALWAYS,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL
+	);
+
+	if (NULL == mLogFile)
+	{
+		return false;
+	}
+
+	SetFilePointer(
+		mLogFile,
+		0,
+		0,
+		FILE_BEGIN
+	);
+
+	// 유니코드 프로젝트라
+	// 파일에 유니코드 형태로 작성된다.
+	// 하지만 그냥 작성하면, 한글이 깨져서 보인다.
+	// 파일이 정확하게 디코딩을 하려면,
+	// 이 파일에 어떤 인코딩 방식이 적용되었는지 알려줘야 하는데,
+	// UTF-16은 문서 선두에 0xFEFF를 넣어주면 된다.
+	unsigned short byteOrderMark{ 0xFEFF };
+	DWORD byteWritten{ 0 };
+
+	WriteFile(
+		mLogFile,
+		&byteOrderMark,
+		sizeof(byteOrderMark),
+		&byteWritten,
+		NULL
+	);
+
+	return true;
+}
+
+bool Log::InitDB()
+{
+	// DB는 사용하게되면
+	// 코드를 추가하자
+
+	return true;
+}
+
+bool Log::InitUDP()
+{
+	WSADATA wsaData{};
+
+	int ret = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (ret)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+bool Log::InitTCP()
+{
+	WSADATA wsaData{};
+
+	// 성공하면 반환 값이 0
+	int ret = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (ret)
+	{
+		return false;
+	}
+
+	// 이미 열려있는 소켓
+	if (INVALID_SOCKET != mTCPSocket)
+	{
+		return false;
+	}
+
+	mTCPSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+	if (INVALID_SOCKET == mTCPSocket)
+	{
+		return false;
+	}
+
+	// 로그를 출력할 곳(연결할 곳)의 정보 세팅
+	SOCKADDR_IN addr{};
+	memset(&addr, 0x00, sizeof(addr));
+	addr.sin_family = AF_INET;
+	inet_pton(AF_INET, mIP, &addr.sin_addr.s_addr);
+	addr.sin_port = htons(mTCPPort);
+
+	ret = connect(mTCPSocket,
+		reinterpret_cast<const sockaddr*>(&addr),
+		sizeof(addr));
+	if (SOCKET_ERROR == ret)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void Log::InsertMsgToQueue(LogMsg* pLogMsg)
+{
+	mLogMsgQueue.Push(pLogMsg);
+}
+
+void Log::OutputFile(wchar_t* outputString)
+{
+	if (NULL == mLogFile)
+	{
+		return;
+	}
+
+	wchar_t strTime[100]{};
+	DWORD fileSize = GetFileSize(mLogFile, NULL);
+
+	// 파일 크기 제한에 걸렸다면
+	if (mFileMaxSize < fileSize || MAX_LOGFILE_SIZE < fileSize)
+	{
+		time_t currTime{ time(NULL) };
+		struct tm localTime {};
+
+		localtime_s(&localTime, &currTime);
+
+		wcsftime(strTime,
+			sizeof(strTime),
+			L"%m월%d일%H시%M분",
+			&localTime);
+
+		// 처음 '_'를 만나기 전까지가 로그 파일의 제목
+		int strIndex = 0;
+		while (mLogFileName[strIndex] != L'_')
+		{
+			++strIndex;
+		}
+		mLogFileName[strIndex] = L'\0';
+
+		swprintf_s(
+			mLogFileName,
+			sizeof(mLogFileName),
+			L"%s_%s.log",
+			mLogFileName,
+			strTime
+		);
+
+		CloseHandle(mLogFile);
+		mLogFile = NULL;
+		InitFile();
+	}
+
+	// 파일 끝으로 파일 포인터 이동
+	SetFilePointer(mLogFile, 0, 0, FILE_END);
+
+	DWORD writtenBytes{ 0 };
+	WriteFile(mLogFile,
+		outputString,
+		static_cast<DWORD>(wcslen(outputString) * 2), // wide char는 2바이트
+		&writtenBytes,
+		NULL);
+}
+
+void Log::OutputDB(wchar_t* outputString)
+{
+	// DB는 사용하게되면 추가
+}
+
+void Log::OutputWindow(eLogInfoType logInfoType, wchar_t* outputString)
+{
+	if (NULL == mHwnd)
+	{
+		return;
+	}
+
+	// 다른 윈도우에게 메시지 전송
+	SendMessage(mHwnd,
+		WM_DEBUGMSG,
+		reinterpret_cast<WPARAM>(outputString),
+		static_cast<LPARAM>(logInfoType));
+}
+
+void Log::OutputDebugger(wchar_t* outputString)
+{
+	// visual studio 출력창에 출력
+	OutputDebugString(outputString);
+}
+
+void Log::OutputUDP(eLogInfoType logInfoType, wchar_t* outputString)
+{
+	SOCKADDR_IN addr;
+	memset(&addr, 0x00, sizeof(addr));
+	addr.sin_family = AF_INET;
+	inet_pton(AF_INET, mIP, &addr.sin_addr.s_addr);
+	addr.sin_port = htons(mUDPPort);
+
+	int bufLength = static_cast<int>(wcslen(outputString));
+
+	if (INVALID_SOCKET == mUDPSocket)
+	{
+		mUDPSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+		if (INVALID_SOCKET == mUDPSocket)
+		{
+			return;
+		}
+	}
+
+	// wchar_t는 2바이트로 표현되기 때문에
+	// 보낼 문자열의 길이 * 2에 해당하는 크기의
+	// 데이터를 보내면 된다.
+	sendto(mUDPSocket,
+		reinterpret_cast<const char*>(outputString),
+		bufLength * sizeof(wchar_t),
+		0,
+		reinterpret_cast<const sockaddr*>(&addr),
+		sizeof(addr));
+}
+
+void Log::OutputTCP(eLogInfoType logInfoType, wchar_t* outputString)
+{
+	int length = static_cast<int>(wcslen(outputString));
+
+	send(mTCPSocket,
+		reinterpret_cast<const char*>(outputString),
+		length * sizeof(wchar_t),
+		0);
+}
+
+bool NETLIB_API INIT_LOG(LogConfig& logConfig)
+{
+	return Log::GetInstance()->Init(logConfig);
+}
+
+void NETLIB_API LOG(eLogInfoType logInfoType, const wchar_t* outputString, ...)
+{
+	Monitor::Owner lock{ gLogSyncObject };
+
+	int queueCount = Log::GetInstance()->GetQueueSize();
+	if (MAX_QUEUECOUNT == queueCount)
+	{
+		return;
+	}
+
+	va_list argPtr{ nullptr };
+
+	// 문자열의 시작 주소를 세팅
+	va_start(argPtr, outputString);
+
+	// argPtr이 문자열을 순회하면서
+	// 서식문자를 만나면,
+	// 뒤에 받은 가변 인자들이 지역 변수로
+	// 스택에 쌓여 있기 때문에
+	// 이를 참조해서 문자열을 완성해준다.
+	vswprintf_s(
+		gLogMsg[queueCount].mOutputString,
+		sizeof(gLogMsg[queueCount].mOutputString),
+		outputString,
+		argPtr);
+
+	va_end(argPtr);
+
+	gLogMsg[queueCount].mLogInfoType = logInfoType;
+
+	// gLogMsg가 배열로 선언되어 있고
+	// 큐의 크기를 index로 해서
+	// gLogMsg[index]에 로그 정보를 세팅하고
+	// 메모리 주소를 큐에 넣어준다.
+	// 일정 주기마다 OnProcess() 함수가 호출되면,
+	// 큐에서 포인터 변수를 꺼내서
+	// 해당하는 메모리에 저장되어 있는 데이터를
+	// 매체에 출력한다.
+	Log::GetInstance()->InsertMsgToQueue(&gLogMsg[queueCount]);
+}
+
+void NETLIB_API LOG_LASTERROR(wchar_t* outputString, ...)
+{
+	va_list argPtr{ nullptr };
+
+	va_start(argPtr, outputString);
+
+	vswprintf_s(
+		gOutString,
+		static_cast<size_t>(sizeof(gOutString) * 0.5),
+		outputString,
+		argPtr
+	);
+
+	va_end(argPtr);
+
+	Log::GetInstance()->LogOutputLastErrorToMsgBox(gOutString);
+}
+
+void NETLIB_API CLOSE_LOG()
+{
+	Log::GetInstance()->CloseAllLog();
+}
---
+++ Practice/NetworkLibrary/Log.h
@@ -0,0 +1,283 @@
+﻿#pragma once
+
+// 2023 08 22 이정모 home
+
+// 게임 서버가 원활하게 돌아가고 있는지
+// 중간에 문제는 없는지
+// 여러 정보들을 남기는 class.
+// server가 패킷을 처리하는 중에 Log를 남기게되면,
+// DB나 File에 log를 출력할텐데,
+// 출력 속도가 느리다 보니 기다리는 시간도 늘어나서 server의 처리량이 감소하게 된다.
+// 그래서 log를 직접 출력하는 것이 아니라 queue에 넣기만 하고
+// 다른 thread가 일정 시간마다 log를 출력하는 방식으로 동작한다.
+
+#define _WINSOCKAPI_
+#include <Windows.h>
+#include <WinSock2.h>
+
+#include "Thread.h"
+#include "Singleton.h"
+#include "Queue.h"
+#include "Monitor.h"
+
+#ifdef NETWORKLIBRARY_EXPORTS
+#define NETLIB_API __declspec(dllexport)
+#else
+#define NETLIB_API __declspec(dllimport)
+#endif
+
+constexpr int MAX_FILENAME_LENGTH = 100;
+constexpr int MAX_IP_LENGTH = 20;
+constexpr int MAX_DSN_NAME = 100;
+constexpr int MAX_DSN_ID = 20;
+constexpr int MAX_DSN_PW = 20;
+constexpr int DEFAULT_TICK = 1000;
+constexpr int DEFAULT_UDPPORT = 1555;
+constexpr int DEFAULT_TCPPORT = 1556;
+constexpr int MAX_OUTPUT_LENGTH = 1024 * 4;
+constexpr int MAX_STORAGE_TYPE = 6;
+constexpr int MAX_QUEUECOUNT = 10000;
+constexpr int MAX_LOGFILE_SIZE = 1024 * 200000; // 200MB
+constexpr int WM_DEBUGMSG = WM_USER + 1;
+
+// 로그를 남길 때
+// 로그의 등급을 eLogInfoType으로 남기는데
+// 이를 문자열과 매칭시켜서 출력하기 위함
+static wchar_t LogInfoType_StringTable[][100] =
+{
+		L"LOG_NONE",
+		L"LOG_INFO_LOW",													//0x00000001
+		L"LOG_INFO_NORMAL",												//0x00000002
+		L"LOG_INFO_LOW , LOG_INFO_NORMAL",								//0x00000003	
+		L"LOG_INFO_HIGH",												//0x00000004	
+		L"LOG_INFO_LOW , LOG_INFO_HIGH",									//0x00000005
+		L"LOG_INFO_NORMAL , LOG_INFO_HIGH",								//0x00000006
+		L"LOG_INFO_LOW , LOG_INFO_NORMAL , LOG_INFO_HIGH",				//0x00000007
+		L"LOG_INFO_CRITICAL",											//0x00000008
+		L"LOG_INFO_LOW , LOG_INFO_CRITICAL",								//0x00000009
+		L"LOG_INFO_NORMAL , LOG_INFO_CRITICAL",							//0x0000000A
+		L"LOG_INFO_LOW , LOG_INFO_NORMAL , LOG_INFO_CRITICAL",			//0x0000000B
+		L"LOG_INFO_HIGH , LOG_INFO_CRITICAL",							//0x0000000C
+		L"LOG_INFO_LOW , LOG_INFO_HIGH , LOG_INFO_CRITICAL",				//0x0000000D
+		L"LOG_INFO_NORMAL , LOG_INFO_HIGH , LOG_INFO_CRITICAL",			//0x0000000E
+		L"LOG_INFO_ALL",													//0x0000000F
+
+		L"LOG_ERROR_LOW",												//0x00000010
+		L"LOG_ERROR_NORMAL",												//0x00000020
+		L"LOG_ERROR_LOW , LOG_ERROR_NORMAL",								//0x00000030	
+		L"LOG_ERROR_HIGH",												//0x00000040	
+		L"LOG_ERROR_LOW , LOG_ERROR_HIGH",								//0x00000050
+		L"LOG_ERROR_NORMAL , LOG_ERROR_HIGH",							//0x00000060
+		L"LOG_ERROR_LOW , LOG_ERROR_NORMAL , LOG_ERROR_HIGH",			//0x00000070
+		L"LOG_ERROR_CRITICAL",											//0x00000080
+		L"LOG_ERROR_LOW , LOG_ERROR_CRITICAL",							//0x00000090
+		L"LOG_ERROR_NORMAL , LOG_ERROR_CRITICAL",						//0x000000A0
+		L"LOG_ERROR_LOW , LOG_ERROR_NORMAL , LOG_ERROR_CRITICAL",		//0x000000B0
+		L"LOG_ERROR_HIGH , LOG_ERROR_CRITICAL",							//0x000000C0
+		L"LOG_ERROR_LOW , LOG_ERROR_HIGH , LOG_ERROR_CRITICAL",			//0x000000D0
+		L"LOG_ERROR_NORMAL , LOG_ERROR_HIGH , LOG_ERROR_CRITICAL",		//0x000000F0 -> 0x000000E0
+		L"LOG_ERROR_ALL",												//0x00000100 -> 0x000000F0
+		L"LOG_ALL"														//0x00000200 -> 0x000000FF	
+};
+
+// 로그의 종류: 알림, 에러
+// 로그의 중요도: 낮음, 보통, 높음, 치명
+// bitwise or 연산을 하기 위해서
+// 값이 2의 n승으로 증가하도록 함.
+// 비트 위치가 겹치지 않기 때문에
+// 특정 비트가 세팅되어 있다는 것이 어떤 고유한 설정임을 말해줌
+enum class eLogInfoType
+{
+	LOG_NONE = 0x00000000,
+
+	LOG_INFO_LOW = 0x00000001,
+	LOG_INFO_NORMAL = 0x00000002,
+	LOG_INFO_HIGH = 0x00000004,
+	LOG_INFO_CRITICAL = 0x00000008,
+	LOG_INFO_ALL = 0x0000000F,
+
+	LOG_ERROR_LOW = 0x00000010,
+	LOG_ERROR_NORMAL = 0x00000020,
+	LOG_ERROR_HIGH = 0x00000040,
+	LOG_ERROR_CRITICAL = 0x00000080,
+	LOG_ERROR_ALL = 0x000000F0,
+
+	LOG_ALL = 0x000000FF,
+};
+
+// 로그를 출력할 매체
+enum class eLogStorageType
+{
+	STORAGE_FILE = 0x00000000,
+	STORAGE_DB = 0x00000001,
+	STORAGE_WINDOW = 0x00000002, // 지정한 윈도우에 메시지 전송
+	STORAGE_OUTPUTWND = 0x00000003, // Debug 출력창
+	STORAGE_UDP = 0x00000004,
+	STORAGE_TCP = 0x00000005,
+};
+
+// 로그를 출력할 파일의 타입
+enum class eLogFileType
+{
+	FILETYPE_NONE = 0x00000000,
+	FILETYPE_XML = 0x00000001,
+	FILETYPE_TEXT = 0x00000002,
+	FILETYPE_ALL = 0x00000003,
+};
+
+struct LogConfig
+{
+	// log를 출력할 매체를 index로 구분하고
+	// 해당 매체에 출력하고 싶은 로그의 등급을 OR 연산해서 세팅
+	int mLogInfoTypes[MAX_STORAGE_TYPE];
+
+	wchar_t mLogFileName[MAX_FILENAME_LENGTH];
+
+	eLogFileType mLogFileType;
+
+	// tcp 및 udp로 로그 출력
+	char mIP[MAX_IP_LENGTH];
+	int mUDPPort;
+	int mTCPPort;
+
+	// DB로 로그를 출력하기 위한 DSN 정보
+	// DB 서버 주소, DB 이름, ID 및 PW, 기타 연결 관련 설정 등
+	char mDSNName[MAX_DSN_NAME];
+	char mDSNID[MAX_DSN_ID];
+	char mDSNPW[MAX_DSN_PW];
+
+	// log를 다른 윈도우에 출력하고 싶을 때가 있을텐데
+	// 출력하고 싶은 윈도우의 핸들이다.
+	HWND mHwnd;
+
+	// log를 출력하는 주기로
+	// mProcessTick마다 OnProcess() 함수 호출
+	DWORD mProcessTick;
+
+	// log 파일 사이즈가 mFileMaxSize보다 크면,
+	// 새로운 파일 생성
+	DWORD mFileMaxSize;
+
+	LogConfig()
+	{
+		ZeroMemory(this, sizeof(LogConfig));
+
+		mProcessTick = DEFAULT_TICK;
+		mUDPPort = DEFAULT_UDPPORT;
+		mTCPPort = DEFAULT_TCPPORT;
+		mFileMaxSize = 1024 * 50000; // 50MB
+	}
+};
+
+// 로그의 등급과
+// 사용자가 작성한 문자열로 이루어져 있으며,
+// 큐에 들어간다.
+struct LogMsg
+{
+	eLogInfoType mLogInfoType;
+	wchar_t mOutputString[MAX_OUTPUT_LENGTH];
+};
+
+// thread class를 상속해서
+// 일정 시간마다 log를 처리하고
+// singleton class를 상속해서
+// 서버 전역에서 필요할 때 log를 남길 수 있도록
+class NETLIB_API Log : public Thread, public Singleton
+{
+	DECLEAR_SINGLETON(Log);
+
+public:
+	// 설정 값을 세팅하자
+	bool Init(LogConfig& logConfig);
+
+	// 실제로 로그를 출력하는 함수
+	void LogOutput(eLogInfoType logInfoType, wchar_t* outputString);
+
+	// 가장 최근에 발생한 에러를 메시지 박스로 출력
+	void LogOutputLastErrorToMsgBox(wchar_t* outputString);
+
+	// 로그 설정들을 초기화하고
+	// 열려있는 자원을 닫아준다.
+	void CloseAllLog();
+
+	// 일정 tick이 지나면,
+	// 부모 class에서 OnProcess()를 호출하는데
+	// 가상 함수 테이블을 참조하여
+	// 자식 class에서 재정의한 OnProcess()가 호출
+	void OnProcess() override;
+
+	// 로그를 출력할 다른 윈도우 핸들 세팅
+	void SetHwnd(HWND hWnd);
+
+	// 로그 메시지를 관리하는 큐의 현재 크기
+	int GetQueueSize();
+
+	// 서버가 연산하고 있는 중에
+	// 로그 출력까지 다 하게 된다면,
+	// 파일이나 DB에 출력하는 경우는 시간이 오래 걸리는 작업이라서
+	// 기다리는데 오랜 시간이 걸린다.
+	// 그래서 Log를 출력하지 않고 queue에 넣어두고
+	// 다른 thread가 일정 시간마다
+	// queue를 검사하여 log를 출력하는 방식으로 동작
+	void InsertMsgToQueue(LogMsg* pLogMsg);
+
+private:
+	// 매체에 로그를 출력하기 위한 동작
+	void OutputFile(wchar_t* outputString);
+	void OutputDB(wchar_t* outputString);
+	void OutputWindow(eLogInfoType logInfoType, wchar_t* outputString);
+	void OutputDebugger(wchar_t* outputString);
+	void OutputUDP(eLogInfoType logInfoType, wchar_t* outputString);
+	void OutputTCP(eLogInfoType logInfoType, wchar_t* outputString);
+
+	// 출력할 매체 초기화
+	bool InitFile();
+	bool InitDB();
+	bool InitUDP();
+	bool InitTCP();
+
+private:
+	// LogConfig를 참조하여 값 세팅
+	int mLogInfoTypes[MAX_STORAGE_TYPE];
+	wchar_t mLogFileName[MAX_FILENAME_LENGTH];
+	eLogFileType mLogFileType;
+
+	char mIP[MAX_IP_LENGTH];
+	int mUDPPort;
+	int mTCPPort;
+
+	char mDSNName[MAX_DSN_NAME];
+	char mDSNID[MAX_DSN_ID];
+	char mDSNPW[MAX_DSN_PW];
+
+	wchar_t mOutString[MAX_OUTPUT_LENGTH];
+	HWND mHwnd;
+
+	HANDLE mLogFile;
+
+	SOCKET mUDPSocket;
+	SOCKET mTCPSocket;
+
+	// 출력할 로그를 모아 놓은 queue
+	// LogMsg가 실제로 존재하는 곳은 gLogMsg
+	Queue<LogMsg*> mLogMsgQueue;
+
+	DWORD mFileMaxSize;
+};
+
+// 로그를 남기고 출력할 때
+// 접근하기 위한 전역 변수
+static wchar_t gOutString[MAX_OUTPUT_LENGTH];
+static LogMsg gLogMsg[MAX_QUEUECOUNT];
+static Monitor gLogSyncObject;
+
+// 로그를 출력하기 위해서 외부에서 사용하는 함수
+
+bool NETLIB_API INIT_LOG(LogConfig& logConfig);
+
+// 로그를 남기는 함수
+void NETLIB_API LOG(eLogInfoType logInfoType, const wchar_t* outputString, ...);
+
+// 가장 최근 에러를 메시지 박스로 출력하는 함수
+void NETLIB_API LOG_LASTERROR(wchar_t* outputString, ...);
+void NETLIB_API CLOSE_LOG();
